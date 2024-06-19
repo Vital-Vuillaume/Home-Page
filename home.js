7f1cb0b701b8498f3cddb0d6a4b906762f58df14
vuillaume <vitalvuillaume@proton.me>
2024-06-19 21:15:52 +0200

    prefer

--- home.js
+++ home.js
@@ -3,16 +3,3 @@
 if (window.location.protocol != "https:") {
   window.location.protocol="https:";
 }
-
-function updateTime() {
-  const now = new Date();
-  const hours = now.getHours().toString().padStart(2, '0');
-  const minutes = now.getMinutes().toString().padStart(2, '0');
-
-  document.getElementById('hours').textContent = hours;
-  document.getElementById('minutes').textContent = minutes;
-}
-
-updateTime();
-
-setInterval(updateTime, 60000);
